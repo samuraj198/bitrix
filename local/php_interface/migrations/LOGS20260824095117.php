36dc3e278c175a881b8ef107a80c0a506ed89812
Даниил Рахимзянов <daniil885@mail.ru>
2026-08-24 06:53:12 +0000

    add migration for IB LOG

---
+++ local/php_interface/migrations/LOGS20260824095117.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class LOGS20260824095117 extends Version
+{
+    protected $author = "samuraj198";
+
+    protected $description = "миграция для 5 задания стажировки";
+
+    protected $moduleVersion = "5.14.0";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        $helper->Iblock()->saveIblockType(array (
+  'ID' => 'logs',
+  'SECTIONS' => 'Y',
+  'EDIT_FILE_BEFORE' => '',
+  'EDIT_FILE_AFTER' => '',
+  'IN_RSS' => 'N',
+  'SORT' => '500',
+  'LANG' => 
+  array (
+    'ru' => 
+    array (
+      'NAME' => 'Логирование',
+      'SECTION_NAME' => '',
+      'ELEMENT_NAME' => '',
+    ),
+    'en' => 
+    array (
+      'NAME' => 'Logs',
+      'SECTION_NAME' => '',
+      'ELEMENT_NAME' => '',
+    ),
+  ),
+));
+        $iblockId = $helper->Iblock()->saveIblock(array (
+  'IBLOCK_TYPE_ID' => 'logs',
+  'LID' => 
+  array (
+    0 => 's1',
+  ),
+  'CODE' => 'LOG',
+  'API_CODE' => NULL,
+  'REST_ON' => 'N',
+  'NAME' => 'Логи',
+  'ACTIVE' => 'Y',
+  'SORT' => '500',
+  'LIST_PAGE_URL' => '#SITE_DIR#/logs/index.php?ID=#IBLOCK_ID#',
+  'DETAIL_PAGE_URL' => '#SITE_DIR#/logs/detail.php?ID=#ELEMENT_ID#',
+  'SECTION_PAGE_URL' => '#SITE_DIR#/logs/list.php?SECTION_ID=#SECTION_ID#',
+  'CANONICAL_PAGE_URL' => '',
+  'PICTURE' => NULL,
+  'DESCRIPTION' => '',
+  'DESCRIPTION_TYPE' => 'text',
+  'RSS_TTL' => '24',
+  'RSS_ACTIVE' => 'Y',
+  'RSS_FILE_ACTIVE' => 'N',
+  'RSS_FILE_LIMIT' => NULL,
+  'RSS_FILE_DAYS' => NULL,
+  'RSS_YANDEX_ACTIVE' => 'N',
+  'XML_ID' => NULL,
+  'INDEX_ELEMENT' => 'Y',
+  'INDEX_SECTION' => 'Y',
+  'WORKFLOW' => 'N',
+  'BIZPROC' => 'N',
+  'SECTION_CHOOSER' => 'L',
+  'LIST_MODE' => 'S',
+  'RIGHTS_MODE' => 'S',
+  'SECTION_PROPERTY' => 'N',
+  'PROPERTY_INDEX' => 'N',
+  'VERSION' => '1',
+  'LAST_CONV_ELEMENT' => '0',
+  'SOCNET_GROUP_ID' => NULL,
+  'EDIT_FILE_BEFORE' => '',
+  'EDIT_FILE_AFTER' => '',
+  'SECTIONS_NAME' => 'Разделы',
+  'SECTION_NAME' => 'Раздел',
+  'ELEMENTS_NAME' => 'Элементы',
+  'ELEMENT_NAME' => 'Элемент',
+  'FULLTEXT_INDEX' => 'N',
+  'EXTERNAL_ID' => NULL,
+  'LANG_DIR' => '/',
+  'IPROPERTY_TEMPLATES' => 
+  array (
+  ),
+  'ELEMENT_ADD' => 'Добавить элемент',
+  'ELEMENT_EDIT' => 'Изменить элемент',
+  'ELEMENT_DELETE' => 'Удалить элемент',
+  'SECTION_ADD' => 'Добавить раздел',
+  'SECTION_EDIT' => 'Изменить раздел',
+  'SECTION_DELETE' => 'Удалить раздел',
+));
+    $helper->Iblock()->saveGroupPermissions($iblockId, array (
+  'administrators' => 'X',
+));
+
+    }
+}
